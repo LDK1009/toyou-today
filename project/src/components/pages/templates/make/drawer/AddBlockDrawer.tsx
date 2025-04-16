bd91c10832f0e419144d43d28e02ece774281c3b
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-04-16 12:50:45 +0900

    ✨ Feat : 텍스트 에디터 정렬, 컬러 상태 관리 UI 개발

--- project/src/components/pages/templates/make/drawer/AddBlockDrawer.tsx
+++ project/src/components/pages/templates/make/drawer/AddBlockDrawer.tsx
@@ -5,12 +5,15 @@ import Box from "@mui/material/Box";
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import { useAddBlockDrawerStore } from "@/store";
 import TextEditor from "./TextEditor";
+import { styled } from "@mui/material";
 
 export default function AddBlockDrawer() {
+  ////////////////////////////// State //////////////////////////////
   const { anchor, isOpen, setIsOpen } = useAddBlockDrawerStore();
 
+  ////////////////////////////// Render //////////////////////////////
   return (
-    <SwipeableDrawer
+    <Container
       anchor={anchor}
       open={isOpen}
       onClose={() => {
@@ -21,19 +24,29 @@ export default function AddBlockDrawer() {
       }}
     >
       <DrawerContent />
-    </SwipeableDrawer>
+    </Container>
   );
 }
+//////////////////////////////////////////////////////////// 스타일
+
+const Container = styled(SwipeableDrawer)`
+  & .MuiPaper-root {
+    border-radius: 0px 0px 8px 8px;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.3);
+    padding: 16px;
+  }
+
+  & .MuiBackdrop-root {
+    background-color: rgba(182, 185, 190, 0.3);
+    backdrop-filter: blur(1px);
+  }
+`;
 
 //////////////////////////////////////////////////////////// 하위 컴포넌트
-
-// 하위 컴포넌트
 const DrawerContent = () => {
-  const { anchor, variant } = useAddBlockDrawerStore();
+  const { variant } = useAddBlockDrawerStore();
 
-  return (
-    <Box sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }} role="presentation">
-      {variant === "text" && <TextEditor />}
-    </Box>
-  );
+  return <DrawerContentContainer role="presentation">{variant === "text" && <TextEditor />}</DrawerContentContainer>;
 };
+
+const DrawerContentContainer = styled(Box)``;
--- project/src/components/pages/templates/make/drawer/TextEditor.tsx
+++ project/src/components/pages/templates/make/drawer/TextEditor.tsx
@@ -1,29 +1,211 @@
-import { Button, TextField } from "@mui/material";
+import { Box, Button, Grid2, Stack, styled, TextField } from "@mui/material";
 import { useMakeTemplateStore } from "@/store/template/makeTemplateStore";
 import { TextBlockType } from "@/types/template/blockType";
 import { useState } from "react";
+import { FormatAlignCenterRounded } from "@mui/icons-material";
+import { FormatAlignRightRounded } from "@mui/icons-material";
+import { FormatAlignLeftRounded } from "@mui/icons-material";
+
 const TextEditor = () => {
   const { addBlock } = useMakeTemplateStore();
 
   const [editBlockState, setEditBlockState] = useState<TextBlockType>({
+    textAlign: "left",
+    color: "#000000",
     text: "",
     fontSize: 1,
-    color: "#000000",
     fontWeight: "normal",
     animation: "none",
-    textAlign: "left",
   });
 
+  // 텍스트 블록 상태 수정 함수
+  function setEditBlockStateProperty(key: string, value: string | number) {
+    setEditBlockState({ ...editBlockState, [key]: value });
+  }
+
   return (
-    <div>
-      <h6>텍스트 에디터</h6>
-      <TextField
-        value={editBlockState.text}
-        onChange={(e) => setEditBlockState({ ...editBlockState, text: e.target.value })}
-      ></TextField>
-      <Button onClick={() => addBlock({ variant: "text", content: editBlockState })}>추가</Button>
-    </div>
+    <Container container>
+      <GridItem size={12}>
+        <h6>미리보기</h6>
+      </GridItem>
+      <GridItem size={6}>
+        <h6>정렬</h6>
+        <AlignPicker currentAlign={editBlockState.textAlign} setEditBlockStateProperty={setEditBlockStateProperty} />
+      </GridItem>
+      <GridItem size={6}>
+        <h6>폰트 사이즈</h6>
+      </GridItem>
+      <GridItem size={6}>
+        <h6>두께</h6>
+      </GridItem>
+      <GridItem size={6}>
+        <h6>애니메이션</h6>
+      </GridItem>
+      <GridItem size={12}>
+        <h6>컬러</h6>
+        <ColorPicker currentColor={editBlockState.color} setEditBlockStateProperty={setEditBlockStateProperty} />
+      </GridItem>
+      <GridItem size={12}>
+        <h6>텍스트</h6>
+        <TextField
+          value={editBlockState.text}
+          onChange={(e) => setEditBlockState({ ...editBlockState, text: e.target.value })}
+        ></TextField>
+        <Button onClick={() => addBlock({ variant: "text", content: editBlockState })}>추가</Button>
+      </GridItem>
+    </Container>
   );
 };
 
 export default TextEditor;
+
+////////////////////////////// 하위 컴포넌트 //////////////////////////////
+////////// 정렬 선택기
+type AlignPickerProps = {
+  currentAlign: string;
+  setEditBlockStateProperty: (key: string, value: string | number) => void;
+};
+
+const AlignPicker = ({ currentAlign, setEditBlockStateProperty }: AlignPickerProps) => {
+  const alignments = [
+    { value: "left", icon: <FormatAlignLeftRounded /> },
+    { value: "center", icon: <FormatAlignCenterRounded /> },
+    { value: "right", icon: <FormatAlignRightRounded /> },
+  ];
+
+  return (
+    <AlignPickerContainer>
+      {alignments.map((alignment, idx) => (
+        <AlignPickerBoxWrapper
+          key={idx}
+          isSelected={alignment.value === currentAlign}
+          onClick={() => setEditBlockStateProperty("textAlign", alignment.value)}
+        >
+          <AlignPickerBox isSelected={alignment.value === currentAlign}>{alignment.icon}</AlignPickerBox>
+        </AlignPickerBoxWrapper>
+      ))}
+    </AlignPickerContainer>
+  );
+};
+
+const AlignPickerContainer = styled(Stack)`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const AlignPickerBoxWrapper = styled(Stack)<{ isSelected: boolean }>`
+  border-width: 2px;
+  border-style: solid;
+  border-color: ${({ theme, isSelected }) => (isSelected ? theme.palette.primary.main : "#EEEEEE")};
+  border-radius: 8px;
+  cursor: pointer;
+
+  transition: all 0.3s ease;
+`;
+
+const AlignPickerBox = styled("div")<{ isSelected: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 24px;
+  height: 24px;
+  margin: 4px;
+  border-radius: 8px;
+
+  & svg {
+    color: ${({ theme, isSelected }) => (isSelected ? theme.palette.text.primary : theme.palette.text.secondary)};
+  }
+`;
+
+////////// 컬러 선택기
+type ColorPickerProps = {
+  currentColor: string;
+  setEditBlockStateProperty: (key: string, value: string | number) => void;
+};
+
+const ColorPicker = ({ currentColor, setEditBlockStateProperty }: ColorPickerProps) => {
+  const colors = [
+    "#FF0000", // 빨강
+    "#FF7F00", // 주황
+    "#FFFF00", // 노랑
+    "#00FF00", // 초록
+    "#0000FF", // 파랑
+    "#4B0082", // 남색
+    "#9400D3", // 보라
+    "#FF1493", // 분홍
+    "#8B4513", // 갈색
+    "#000000", // 검정
+    "#808080", // 회색
+    "#FFFFFF", // 흰색
+  ];
+
+  return (
+    <ColorPickerContainer>
+      {colors.map((color, idx) => (
+        <ColorPickerBoxWrapper
+          key={idx}
+          color={color}
+          isSelected={color === currentColor}
+          onClick={() => setEditBlockStateProperty("color", color)}
+        >
+          <ColorPickerBox color={color} />
+        </ColorPickerBoxWrapper>
+      ))}
+    </ColorPickerContainer>
+  );
+};
+
+const ColorPickerContainer = styled(Stack)`
+  flex-direction: row;
+  column-gap: 4px;
+  width: 100%;
+  overflow-x: scroll;
+  /* Chrome, Safari, Opera*/
+  ::-webkit-scrollbar {
+    display: none;
+  }
+
+  /* Firefox */
+  scrollbar-width: none;
+
+  /* IE and Edge */
+  -ms-overflow-style: none;
+`;
+
+const ColorPickerBoxWrapper = styled("div")<{ color: string; isSelected: boolean }>`
+  border-width: 2px;
+  border-style: solid;
+  border-color: ${({ isSelected, color }) => (isSelected ? color : "#EEEEEE")};
+  border-radius: 8px;
+  cursor: pointer;
+
+  transition: all 0.3s ease;
+`;
+
+const ColorPickerBox = styled("div")<{ color: string }>`
+  width: 24px;
+  height: 24px;
+  margin: 4px;
+  background-color: ${({ color }) => color};
+  border-radius: 8px;
+`;
+
+////////// 폰트 사이즈 선택기
+const FontSizePicker = () => {
+  return <div>FontSizePicker</div>;
+};
+
+////////// 두께 선택기
+const FontWeightPicker = () => {
+  return <div>FontWeightPicker</div>;
+};
+
+////////// 애니메이션 선택기
+const AnimationPicker = () => {
+  return <div>AnimationPicker</div>;
+};
+
+////////////////////////////// 스타일 //////////////////////////////
+const Container = styled(Grid2)``;
+
+const GridItem = styled(Grid2)``;
