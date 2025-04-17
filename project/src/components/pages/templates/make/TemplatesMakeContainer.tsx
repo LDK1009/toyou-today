a283159de1a09dc648f491267dd0a828d0c4139a
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-04-17 14:36:16 +0900

    ✨ Feat : 블럭 추가 버튼 UI 개발

--- project/src/components/pages/templates/make/TemplatesMakeContainer.tsx
+++ project/src/components/pages/templates/make/TemplatesMakeContainer.tsx
@@ -1,26 +1,32 @@
 "use client";
 
 import { mixinContainer } from "@/styles/mixins";
-import { AddCircleOutlineRounded } from "@mui/icons-material";
-import { Box, Button, Typography } from "@mui/material";
+import {
+  AddCircleOutlineRounded,
+  CalendarMonthRounded,
+  ImageRounded,
+  InsertLinkRounded,
+  TextFormatRounded,
+  VideocamRounded,
+} from "@mui/icons-material";
+import { Box, Button, Fade, Stack, Typography } from "@mui/material";
 
 import { styled } from "@mui/material";
 import AddBlockDrawer from "./drawer/AddBlockDrawer";
 import { useAddBlockDrawerStore } from "@/store";
 import { useMakeTemplateStore } from "@/store/template/makeTemplateStore";
 import TextBlock from "../block/TextBlock";
-import React from "react";
+import React, { useState } from "react";
+import { BlockVariantType } from "@/types/template/blockType";
+
 const TemplatesMakeContainer = () => {
   ////////////////////////////////////////////////// Store
   // AddBlockDrawerStore
-  const { setAllState: setAddBlockDrawerState } = useAddBlockDrawerStore();
+
   // MakeTemplateStore
   const { templateBlocks } = useMakeTemplateStore();
 
   ////////////////////////////////////////////////// function
-  const handleOpenDrawer = () => {
-    setAddBlockDrawerState("text", "top", true);
-  };
 
   const renderBlocks = () => {
     return templateBlocks.map((el, index) => {
@@ -37,15 +43,94 @@ const TemplatesMakeContainer = () => {
 
       {renderBlocks()}
 
-      <Button onClick={handleOpenDrawer} variant="outlined" fullWidth endIcon={<AddCircleOutlineRounded />}>
-        블럭 생성
-      </Button>
+      <AddBlockButton />
     </Container>
   );
 };
 
 export default TemplatesMakeContainer;
 
+////////////////////////////// 하위 컴포넌트 //////////////////////////////
+const AddBlockButton = () => {
+  const { setAllState: setAddBlockDrawerState } = useAddBlockDrawerStore();
+  const [isFadeShow, setIsFadeShow] = useState(false);
+
+  const handleOpenDrawer = (variant: BlockVariantType) => {
+    setAddBlockDrawerState(variant, "top", true);
+    setIsFadeShow(false);
+  };
+
+  const buttons: { variant: BlockVariantType; icon: React.ReactNode }[] = [
+    {
+      variant: "text",
+      icon: <TextFormatRounded />,
+    },
+    {
+      variant: "calendar",
+      icon: <CalendarMonthRounded />,
+    },
+    {
+      variant: "image",
+      icon: <ImageRounded />,
+    },
+    {
+      variant: "video",
+      icon: <VideocamRounded />,
+    },
+    {
+      variant: "link",
+      icon: <InsertLinkRounded />,
+    },
+  ];
+
+  return (
+    <AddBlockButtonContainer>
+      <Fade in={isFadeShow}>
+        <MenuButtonContainer>
+          {buttons.map((el, index) => (
+            <MenuButton onClick={() => handleOpenDrawer(el.variant)} key={index} variant="outlined" endIcon={el.icon} />
+          ))}
+        </MenuButtonContainer>
+      </Fade>
+
+      <Button
+        onClick={() => setIsFadeShow(!isFadeShow)}
+        variant="outlined"
+        fullWidth
+        endIcon={<AddCircleOutlineRounded />}
+      >
+        {isFadeShow ? "취소" : "블럭 생성"}
+      </Button>
+    </AddBlockButtonContainer>
+  );
+};
+
+const AddBlockButtonContainer = styled(Stack)`
+  row-gap: 8px;
+`;
+
+const MenuButtonContainer = styled(Stack)`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  column-gap: 8px;
+`;
+
+const MenuButton = styled(Button)`
+  width: 32px;
+  height: 32px;
+  min-width: 32px;
+  max-width: 32px;
+  border-radius: 50%;
+  border: 1px solid ${({ theme }) => theme.palette.primary.main};
+  padding: 0;
+
+  & .MuiButton-icon {
+    margin: 0;
+  }
+`;
+
+////////////////////////////// 스타일 컴포넌트 //////////////////////////////
 const Container = styled(Box)`
   ${mixinContainer()}
 `;
