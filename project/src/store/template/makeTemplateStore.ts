2870f3c0edc87a516346eff03ac9ef2fecbe08ea
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-04-28 23:39:12 +0900

    ✨ Feat : 블록 이동 액션 추가

--- project/src/store/template/makeTemplateStore.ts
+++ project/src/store/template/makeTemplateStore.ts
@@ -4,20 +4,32 @@ import { MakeTemplateType } from "@/types/template/templateType";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
+// 스토어 타입 정의
 interface StoreType {
-  // 상태
+  ////////// 상태
+  // 템플릿 상태
   template: MakeTemplateType;
 
-  // 페이지 에셋 관련 액션
+  ////////// 페이지 에셋 관련 액션
+  // 페이지 에셋 설정
   setPageAsset: (name: PageAssetVariantType, content: PageAssetContentType) => void;
 
-  // 블록 관련 액션
-  addBlock: (block: BlockType) => void;
-  deleteBlock: (index: number) => void;
+  ////////// 블록 관련 액션
+  // 블록 세팅
   setTemplateBlocks: (templateBlocks: BlockType[]) => void;
+  // 블록 추가
+  addBlock: (block: BlockType) => void;
+  // 블록 수정
+  updateBlock: (index: number, block: BlockType) => void;
+  // 블록 삭제
+  deleteBlock: (index: number) => void;
+  // 블록 한 칸 위로 옮기기
+  moveUpBlock: (targetBlockIndex: number) => void;
+  // 블록 한 칸 아래로 옮기기
+  moveDownBlock: (targetBlockIndex: number) => void;
 }
 
-// 초기 템플릿
+// 템플릿 초기 상태값
 const initialTemplate: MakeTemplateType = {
   name: "",
   makerId: "",
@@ -29,24 +41,35 @@ const initialTemplate: MakeTemplateType = {
   blocks: [],
 };
 
+// 스토어 생성
 export const useMakeTemplateStore = create<StoreType>()(
   persist(
     (set) => ({
-      // 상태
+      ////////// 상태
       template: initialTemplate,
 
-      // 액션(페이지 에셋)
+      ////////// 페이지 에셋 관련 액션
       // 페이지 에셋 설정
       setPageAsset: (name, content) =>
         set((state) => ({
           template: { ...state.template, pageAssets: { ...state.template.pageAssets, [name]: content } },
         })),
 
-      // 액션(블록)
-      // 액션(블록)
+      ////////// 블록 관련 액션
+
+      // 블록 세팅
+      setTemplateBlocks: (templateBlocks: BlockType[]) =>
+        set((state) => ({ template: { ...state.template, blocks: templateBlocks } })),
+
       // 블록 추가
       addBlock: (block: BlockType) =>
         set((state) => ({ template: { ...state.template, blocks: [...state.template.blocks, block] } })),
+
+      // 블록 수정
+      updateBlock: (index, block) =>
+        set((state) => ({
+          template: { ...state.template, blocks: state.template.blocks.map((b, i) => (i === index ? block : b)) },
+        })),
 
       // 블록 삭제
       deleteBlock: (index: number) =>
@@ -54,9 +77,53 @@ export const useMakeTemplateStore = create<StoreType>()(
           template: { ...state.template, blocks: state.template.blocks.filter((_, i) => i !== index) },
         })),
 
-      // 블록 설정
-      setTemplateBlocks: (templateBlocks: BlockType[]) =>
-        set((state) => ({ template: { ...state.template, blocks: templateBlocks } })),
+      // 블록 한 칸 위로 옮기기
+      moveUpBlock: (targetBlockIndex: number) =>
+        set((state) => ({
+          template: {
+            ...state.template,
+            blocks: state.template.blocks.map((el, idx) => {
+              // 임시 변수에 타겟 블록 저장
+              const tempBlock = state.template.blocks[targetBlockIndex];
+
+              // 타겟 인덱스 위치에 윗 블록 대입
+              if (idx === targetBlockIndex) {
+                return state.template.blocks[idx - 1];
+              }
+
+              // 타겟 인덱스 윗 블록 위치에 타겟 블록 대입
+              if (idx === targetBlockIndex - 1) {
+                return tempBlock;
+              }
+
+              return el;
+            }),
+          },
+        })),
+
+      // 블록 한 칸 아래로 옮기기
+      moveDownBlock: (targetBlockIndex: number) =>
+        set((state) => ({
+          template: {
+            ...state.template,
+            blocks: state.template.blocks.map((el, idx) => {
+              // 임시 변수에 타겟 블록 저장
+              const tempBlock = state.template.blocks[targetBlockIndex];
+
+              // 타겟 인덱스 위치에 윗 블록 대입
+              if (idx === targetBlockIndex) {
+                return state.template.blocks[idx + 1];
+              }
+
+              // 타겟 인덱스 윗 블록 위치에 타겟 블록 대입
+              if (idx === targetBlockIndex + 1) {
+                return tempBlock;
+              }
+
+              return el;
+            }),
+          },
+        })),
     }),
     {
       name: "makeTemplate-storage",
