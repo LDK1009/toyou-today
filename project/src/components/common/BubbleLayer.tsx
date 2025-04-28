0048fc62d94438f0fa8301ebbc78cf54cd76b1ab
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-04-28 12:33:04 +0900

    ✨ Feat : 롤링페이퍼 입력 컴포넌트 개발

--- project/src/components/common/BubbleLayer.tsx
+++ project/src/components/common/BubbleLayer.tsx
@@ -106,8 +106,8 @@ const BubbleLayer = () => {
           position: "fixed",
           top: 0,
           left: 0,
-          width: "100%",
-          height: "100%",
+          width: "100vw",
+          height: "100vh",
           zIndex: 100,
           pointerEvents: "none", // 이벤트 통과 설정
         }}
--- project/src/components/pages/templates/page-asset/RollingPaperPageAsset.tsx
+++ project/src/components/pages/templates/page-asset/RollingPaperPageAsset.tsx
@@ -6,24 +6,31 @@ import {
   RollingPaperAsset_EmpathyItemType,
 } from "@/types/template/pageAssetType";
 import CommonTab from "@/components/common/CommonTab";
-import { Button, Grid2, Stack, styled, Typography } from "@mui/material";
+import { Button, Grid2, Stack, styled, TextField, Typography } from "@mui/material";
 import { mixinHideScrollbar } from "@/styles/mixins";
 import { motion, useInView } from "motion/react";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import BubbleLayer from "@/components/common/BubbleLayer";
 import { HistoryEduRounded } from "@mui/icons-material";
 import { enqueueSnackbar } from "notistack";
+import { shouldForwardProp } from "@/utils/mui";
 
-const RollingPaperPageAsset = ({ pageAssetData }: { pageAssetData: RollingPaperAssetType }) => {
+const RollingPaperPageAsset = ({
+  pageAssetData,
+  preview = false,
+}: {
+  pageAssetData: RollingPaperAssetType;
+  preview?: boolean;
+}) => {
   // 탭 리스트
   const tabList = [
     {
       label: "댓글",
-      component: <CommentTab comments={pageAssetData.comments} />,
+      component: <CommentTab comments={pageAssetData.comments} preview={preview} />,
     },
     {
       label: "공감",
-      component: <EmpathyTab empathies={pageAssetData.empathies} />,
+      component: <EmpathyTab empathies={pageAssetData.empathies} preview={preview} />,
     },
   ];
 
@@ -37,8 +44,9 @@ const RollingPaperPageAsset = ({ pageAssetData }: { pageAssetData: RollingPaperA
 export default RollingPaperPageAsset;
 
 //////////////////////////////////////// 스타일 ////////////////////////////////////////
-const Container = styled(Box)`
+const Container = styled(Stack)`
   width: 100%;
+  row-gap: 16px;
 `;
 
 //////////////////////////////////////// 하위 컴포넌트 ////////////////////////////////////////
@@ -79,33 +87,45 @@ const ItemAnimation = ({
 //////////////////// 댓글 탭 ////////////////////
 type CommentTabPropsType = {
   comments: RollingPaperAsset_CommentItemType[];
+  preview?: boolean;
 };
 
-const CommentTab = ({ comments }: CommentTabPropsType) => {
+const CommentTab = ({ comments, preview }: CommentTabPropsType) => {
   const inViewRef = useRef(null);
   const isInView = useInView(inViewRef);
 
   return (
     <CommentTab_Container ref={inViewRef}>
-      <ReadLetterModeButton
-        variant="outlined"
-        startIcon={<HistoryEduRounded />}
-        onClick={() => enqueueSnackbar("개발 중인 기능입니다.", { variant: "info" })}
-      >
-        편지 모드로 보기
-      </ReadLetterModeButton>
-      {comments.map((item, idx) => (
-        <CommentItem key={idx} {...item} isCommentTabView={isInView} index={idx} />
-      ))}
+      {/* 댓글 목록 섹션 */}
+      <CommentTab_CommentSection>
+        <ReadLetterModeButton
+          variant="outlined"
+          startIcon={<HistoryEduRounded />}
+          onClick={() => enqueueSnackbar("개발 중인 기능입니다.", { variant: "info" })}
+        >
+          편지 모드로 보기
+        </ReadLetterModeButton>
+        {comments.map((item, idx) => (
+          <CommentItem key={idx} {...item} isCommentTabView={isInView} index={idx} />
+        ))}
+      </CommentTab_CommentSection>
+
+      {/* 댓글 입력 섹션 */}
+      <CommentInputSection preview={preview} />
     </CommentTab_Container>
   );
 };
 
 const CommentTab_Container = styled(Stack)`
   width: 100%;
-  row-gap: 8px;
+  row-gap: 16px;
   padding-top: 16px;
-  height: 300px;
+`;
+
+const CommentTab_CommentSection = styled(Stack)`
+  width: 100%;
+  row-gap: 8px;
+  height: 320px;
   overflow-y: auto;
   ${mixinHideScrollbar}
 `;
@@ -145,26 +165,108 @@ const CommentItem_Nickname = styled(Typography)`
 const CommentItem_Comment = styled(Typography)`
   color: ${({ theme }) => theme.palette.text.primary};
 `;
+//////////////////// 댓글 입력 섹션 ////////////////////
+const CommentInputSection = ({ preview }: { preview?: boolean }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmitButtonClick = () => {
+    // 미리보기 모드 예외 처리
+    if (preview) {
+      enqueueSnackbar("미리보기 모드에서는 댓글을 입력할 수 없습니다.", { variant: "info" });
+      return;
+    }
+
+    // 입력값 없음 예외 처리
+    if (inputValue.trim() === "") {
+      enqueueSnackbar("댓글을 입력해주세요.", { variant: "error" });
+      return;
+    }
+
+    /**
+     * 댓글 제출
+     * 1. 기존 댓글 상태 가져오기
+     * 2. 새로운 댓글 추가
+     * 3. DB에 댓글 추가
+     * */
+    console.log(inputValue);
+
+    return;
+  };
+
+  return (
+    <CommentInputSection_Container container spacing={1}>
+      <Grid2 size={9.5}>
+        <CommentInputSection_Input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="댓글을 입력해주세요."
+        />
+      </Grid2>
+      <Grid2 size={2.5}>
+        <CommentInputSection_Button onClick={handleSubmitButtonClick} variant="contained">
+          <HistoryEduRounded />
+        </CommentInputSection_Button>
+      </Grid2>
+    </CommentInputSection_Container>
+  );
+};
+
+const CommentInputSection_Container = styled(Grid2)``;
+
+const CommentInputSection_Input = styled(TextField)`
+  width: 100%;
+  & .MuiInputBase-root {
+    height: 40px;
+    border: 1px solid ${({ theme }) => theme.palette.primary.main};
+  }
+
+  & .MuiOutlinedInput-notchedOutline {
+    border-color: ${({ theme }) => theme.palette.primary.main};
+  }
+`;
+
+const CommentInputSection_Button = styled(Button)`
+  width: 100%;
+  height: 100%;
+  box-shadow: none;
+
+  & .MuiSvgIcon-root {
+    color: ${({ theme }) => theme.palette.text.white};
+  }
+`;
 
 //////////////////// 공감 탭 ////////////////////
-const EmpathyTab = ({ empathies }: { empathies: RollingPaperAsset_EmpathyItemType[] }) => {
+type EmpathyTabPropsType = {
+  empathies: RollingPaperAsset_EmpathyItemType[];
+  preview?: boolean;
+};
+
+const EmpathyTab = ({ empathies, preview }: EmpathyTabPropsType) => {
   const inViewRef = useRef(null);
   const isInView = useInView(inViewRef);
   return (
     <EmpathyTab_Container ref={inViewRef}>
-      <EmpathyItemWrapper container spacing={1}>
-        {empathies.map((item, idx) => (
-          <EmpathyItem key={idx} {...item} isEmpathyTabView={isInView} index={idx} />
-        ))}
-      </EmpathyItemWrapper>
+      <EmpathyTab_EmpathySection>
+        <EmpathyItemWrapper container spacing={1}>
+          {empathies.map((item, idx) => (
+            <EmpathyItem key={idx} {...item} isEmpathyTabView={isInView} index={idx} />
+          ))}
+        </EmpathyItemWrapper>
+      </EmpathyTab_EmpathySection>
+      <EmpathyInputSection preview={preview} />
       {isInView && <BubbleLayer />}
     </EmpathyTab_Container>
   );
 };
 
-const EmpathyTab_Container = styled(Box)`
+const EmpathyTab_Container = styled(Stack)`
   width: 100%;
-  height: 300px;
+  row-gap: 16px;
+`;
+
+const EmpathyTab_EmpathySection = styled(Box)`
+  width: 100%;
+  height: 320px;
   overflow-y: auto;
   ${mixinHideScrollbar}
 
@@ -207,3 +309,76 @@ const EmpathyItem_Container = styled(Stack)`
 const EmpathyItem_Nickname = styled(CommentItem_Nickname)``;
 
 const EmpathyItem_Empathy = styled(CommentItem_Comment)``;
+
+//////////////////// 공감 입력 섹션 ////////////////////
+const EmpathyInputSection = ({ preview }: { preview?: boolean }) => {
+  console.log(preview);
+
+  const emojiList = ["🎉", "🎊", "🔥", "👍🏻", "💕"];
+
+  const [inputValue, setInputValue] = useState("");
+  const [bubbleState, setBubbleState] = useState(false);
+
+  useEffect(() => {
+    if (inputValue) {
+      setBubbleState(true);
+    } else {
+      setBubbleState(false);
+    }
+  }, [inputValue]);
+
+  const handleEmojiButtonClick = (item: string) => {
+    // 현재 입력한 이모지를 클릭했다면
+    if (item === inputValue) {
+      setInputValue("");
+      return;
+    } else {
+      setInputValue(item);
+      /**
+       * 1. 기존 공감 목록 가져오기
+       * 2. 공감 목록에 이미 사용자의 동일한 이모지가 있다면 함수 종료
+       * 3. 공감 목록에 사용자의 동일한 이모지가 없다면 공감 상태에 추가
+       * 4. DB에 공감 추가
+       */
+    }
+  };
+
+  return (
+    <EmpathyInputSection_Container>
+      {emojiList.map((item, idx) => (
+        <EmpathyInputSection_EmpathyButton
+          key={idx}
+          onClick={() => handleEmojiButtonClick(item)}
+          $isSelected={item === inputValue}
+        >
+          {item}
+        </EmpathyInputSection_EmpathyButton>
+      ))}
+      {bubbleState && <BubbleLayer />}
+    </EmpathyInputSection_Container>
+  );
+};
+
+const EmpathyInputSection_Container = styled(Stack)`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px;
+  border: 1px solid ${({ theme }) => theme.palette.primary.main};
+  border-radius: 8px;
+`;
+
+type EmpathyInputSection_EmpathyButtonPropsType = {
+  $isSelected: boolean;
+};
+
+const EmpathyInputSection_EmpathyButton = styled(Button, {
+  shouldForwardProp,
+})<EmpathyInputSection_EmpathyButtonPropsType>`
+  min-width: 40px;
+  min-height: 40px;
+  border-radius: 50%;
+  border: 1px solid ${({ theme, $isSelected }) => ($isSelected ? theme.palette.primary.light : "transparent")};
+  transform: ${({ $isSelected }) => ($isSelected ? "scale(1.5)" : "scale(1)")};
+  transition: all 0.3s ease-in-out;
+`;
